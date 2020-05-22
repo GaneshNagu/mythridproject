a9b5519ebb9628f020767cd7a77238e6527d017d
Nagaraju <rajunaga57@gmail.com>
2020-05-22 23:05:00 +0530

    saving unsaved changes

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -1,77 +1,81 @@
-import React,{useEffect} from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Cockpit.css';
 
 
-const Cockpit=(props)=>{
-  useEffect(()=>{
-    console.log('[Cockpit.js] UseEffect called');
-    setTimeout(()=>{
-      alert('data saved to server');
-    },1000)
-    return()=>{
+const Cockpit = (props) => {
+
+  const Togglerefr = useRef(null);
+
+  useEffect(() => {
+    // console.log('[Cockpit.js] UseEffect called');
+    // setTimeout(()=>{
+    //   alert('data saved to server');
+    // },1000)
+    Togglerefr.current.click();
+    return () => {
       console.log('[cockpit.js] cleanup work  called');
     }
-  },[]
+  }, []
   )
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log('[Cockpit.js] UseEffect 2nd called');
-        return()=>{
+    return () => {
       console.log('[cockpit.js] cleanup 2nd work  called');
     }
   });
 
 
-    let textpass = null;
+  let textpass = null;
 
-    const stylepass = [];
+  const stylepass = [];
 
-    if (props.personslength === 2) {
-      stylepass.push('red');
-      textpass = 'we have only two elements';
+  if (props.personslength === 2) {
+    stylepass.push('red');
+    textpass = 'we have only two elements';
+  }
+
+  if (props.personslength === 3) {
+    stylepass.push('bold');
+    textpass = 'we have three elements ';
+  }
+
+  if (props.personslength === 1) {
+    stylepass.push('under');
+    textpass = "we have only 1 element"
+
+  }
+
+  if (props.personslength === 4) {
+    stylepass.push('backgr');
+    textpass = 'we have four elements';
+  }
+
+
+  const style = {
+    backgroundColor: 'Green',
+    color: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '10px',
+    cursor: 'pointer',
+    Margin: '30px',
+    borderRadius: '20px',
+    ':hover': {
+      border: '3px solid blue'
     }
+  }
 
-    if (props.personslength === 3) {
-      stylepass.push('bold');
-      textpass = 'we have three elements ';
-    }
+  return (
 
-    if (props.personslength === 1) {
-      stylepass.push('under');
-      textpass = "we have only 1 element"
-
-    }
-
-    if (props.personslength === 4) {
-      stylepass.push('backgr');
-      textpass = 'we have four elements';
-    }
-    
-
-    const style = {
-        backgroundColor: 'Green',
-        color: 'white',
-        font: 'inherit',
-        border: '1px solid blue',
-        padding: '10px',
-        cursor: 'pointer',
-        Margin: '30px',
-        borderRadius: '20px',
-        ':hover': {
-          border: '3px solid blue'
-        }
-      }
-
-    return(
-
-        <div>
-        <p className={stylepass}>{textpass}</p><br />
-        <button onClick={props.toggle} style={style}>click to change</button>
-        </div>
+    <div>
+      <p className={stylepass}>{textpass}</p><br />
+      <button onClick={props.toggle} ref={Togglerefr} style={style}>click to change</button>
+    </div>
 
 
 
-    );
+  );
 }
 
 
