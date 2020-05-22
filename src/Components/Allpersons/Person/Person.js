774fdf12ea9f18f3d3d717470198509542164ce2
Nagaraju <rajunaga57@gmail.com>
2020-05-22 23:10:28 +0530

    cvbvbcvbvcbvb

--- src/Components/Allpersons/Person/Person.js
+++ src/Components/Allpersons/Person/Person.js
@@ -1,80 +1,49 @@
-
-import React, { Component } from 'react';
-import Withclass from '../../../HOC/Withclass';
-import './Person.css';
-
-// import React, { Component } from 'react';
-import propTypes from 'prop-types';
-// >>>>>>> 9a075078db5a7e033ec37b6dbc7cf96d053f371d
-
+import React ,{Component} from 'react';
 // import { render } from 'react-dom';
-
+ 
 // import Radium from 'radium';
-
+// // import './Person.css';
 // import Styled from 'styled-components';
 
 
 class Person extends Component {
-    constructor(props) {
-        super(props);
-        this.inputelemref = React.createRef();
+    render(){
+    const style={
+        textAlign: 'center',
+        width: '60%',
+        height: '150px',
+        margin:'16px auto',
+        border: '1px solid black',
+        borderRadius: '15px'
+        // @media (min-width:500px): {
+        //     width: 450px;,
+        // };
     }
+    // const Styleddiv = Styled.div`
+    //     text-align: center;
+    //     width: 60%;
+    //     height: 150px;
+    //     margin:16px auto;
+    //     border: 1px solid black;
+    //     border-radius: 15px;
+    //     @media (min-width:500px): {
+    //         width: 450px;
+    //     };
+    //     `;
+
+    return (
+         < div className="custclas" style={style}>
+        
+            <h1 onClick={this.props.click}>{this.props.name}</h1>
+            <p>Iam {this.props.age} years old</p>
+            <input type="text" value={this.props.name} onChange={this.props.changed} />
+            {/* <Styleddiv></Styleddiv> */}
+         </div>
 
 
-    componentDidMount() {
-        this.inputelemref.current.focus();
-    }
-
-    render() {
-        const custclas = {
-            textAlign: 'center',
-            width: '60%',
-            height: '150px',
-            margin: '16px auto',
-            border: '1px solid black',
-            borderRadius: '15px'
-            // @media (min-width:500px): {
-            //     width: 450px;,
-            // };
-        }
-        // const Styleddiv = Styled.div`
-        //     text-align: center;
-        //     width: 60%;
-        //     height: 150px;
-        //     margin:16px auto;
-        //     border: 1px solid black;
-        //     border-radius: 15px;
-        //     @media (min-width:500px): {
-        //         width: 450px;
-        //     };
-        //     `;
-
-        // className = "custclas" style = { style }
-
-        return (
-            <Withclass classes={custclas} >
-
-                <h1 onClick={this.props.click}>{this.props.name}</h1>
-                <p>Iam {this.props.age} years old</p>
-                <input type="text"
-                    ref={this.inputelemref}
-                    value={this.props.name}
-                    onChange={this.props.changed} />
-                {/* <Styleddiv></Styleddiv> */}
-            </Withclass>
+    )
 
 
-        )
-
-
-    }
-}
-
-Person.propTypes = {
-    click: propTypes.func,
-    name: propTypes.string,
-    age: propTypes.number,
-    changed: propTypes.func
-}
+}}
 
 export default Person;
--- src/HOC/Withclass.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Withclass = (props) => {
-    return (
-        <div className={props.classes}>{props.children}</div>
-    )
-}
-
-export default Withclass; 
